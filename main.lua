7102f5f54c2fbf508612c7da04cb432c2de2b9fd
Justin van der Leij <justinvanderleij@gmail.com>
2018-08-03 13:52:37 +0200

    Fixed EVERYTHING

--- main.lua
+++ main.lua
@@ -1,58 +1,111 @@
-local cpml = require("cpml")
-
-local ModelMatrix = cpml.mat4.identity()
-local ViewMatrix  = cpml.mat4.identity()
-ViewMatrix:translate(ViewMatrix, cpml.vec3(4, 4, 3))
-ViewMatrix:look_at(ViewMatrix,  cpml.vec3(4, 4, 3), cpml.vec3(0, 0, 0), cpml.vec3(0, 1, 0))
-
-local PerspectiveMatrix = cpml.mat4.from_perspective (
-   90, 
-   16 / 9,
-   0.1,
-   10000.0
-)
-
+--CPML is the magic that makes the transforms work
+local Cpml = require("cpml")
 local Shader = love.graphics.newShader("shader.glsl")
 
-local Mesh = love.graphics.newMesh({
-   {"VertexPosition", "float", 3},
-   {"VertexTexCoord", "float", 2},
-   {"VertexColor",    "byte",  4},
-}, {
-   { 0,  1, 0, 0, 0, 1, 1, 1, 1},
-   {-1, -1, 0, 0, 0, 1, 1, 1, 1},
-   { 1, -1, 0, 0, 0, 1, 1, 1, 1},
-}, "triangles", "dynamic")
+local DepthBuffer = {
+   color = love.graphics.newCanvas(w, h, {format = "rgba8"}),
+   depth = love.graphics.newCanvas(w, h, {format = "depth24"}),
+}
+DepthBuffer.canvas = {DepthBuffer.color, depthstencil = DepthBuffer.depth}
 
-function love.update(dt)
-   local delta = cpml.vec3(0, 0, 0)
-   local rdelta = 0
 
-   if love.keyboard.isDown("a") then delta.x = delta.x + 1 end
-   if love.keyboard.isDown("d") then delta.x = delta.x - 1 end
+local VertexFormat = {
+	{"VertexPosition", "float", 3},
+   {"VertexTexCoord","float", 2},
+   {"VertexColor", "byte", 4},
+}
 
-   if love.keyboard.isDown("w") then delta.z = delta.z + 1 end
-   if love.keyboard.isDown("s") then delta.z = delta.z - 1 end
+local Mesh = love.graphics.newMesh(VertexFormat, {
+   {-1, -1, 0, 0, 1, 255, 255, 255, 255},
+   { 1, -1, 0, 1, 1, 255, 255, 255, 255},
+   {-1,  1, 0, 0, 0, 255, 255, 255, 255},
+   { 1,  1, 0, 1, 0, 255, 255, 255, 255},
+}, "triangles")
+Mesh:setVertexMap({1, 2, 3, 2, 3, 4})
+Mesh:setTexture(love.graphics.newImage("stone.png"))
 
-   if love.keyboard.isDown("r") then delta.y = delta.y - 1 end
-   if love.keyboard.isDown("f") then delta.y = delta.y + 1 end
+local Camera = {
+   position = Cpml.vec3(0, 0, -10),
+   rotation = Cpml.vec2(0, 0),
+}
 
-   if love.keyboard.isDown("q") then rdelta = rdelta - 3 end
-   if love.keyboard.isDown("e") then rdelta = rdelta + 3 end
+local PerspectiveMatrix = Cpml.mat4.from_perspective(
+   75, 
+   love.graphics.getWidth() / love.graphics.getHeight(), 
+   0.1, 
+   1000
+)
+local ViewMatrix = Cpml.mat4()
 
-   ViewMatrix:translate(ViewMatrix, delta * dt)
-   ViewMatrix:rotate(ViewMatrix, 5 * dt, cpml.vec3(0, 1, 0))
-end
+local ModelMatrix_1 = Cpml.mat4.identity()
+ModelMatrix_1:translate(ModelMatrix_1, Cpml.vec3(1, 0, 0))
 
+local ModelMatrix_2 = Cpml.mat4.identity()
+ModelMatrix_2:translate(ModelMatrix_2, Cpml.vec3(5, 0, 2))
 
 function love.draw()
-   --ViewMatrix:invert(ViewMatrix)
+   local ViewMatrix = ViewMatrix:identity()
 
-   Shader:send("ModelMatrix", "row", ModelMatrix)
-   Shader:send("ViewMatrix", ViewMatrix)
-   Shader:send("PerspectiveMatrix", "row", PerspectiveMatrix)
+	ViewMatrix:translate(ViewMatrix, Camera.position)
+	ViewMatrix:rotate   (ViewMatrix, Camera.rotation.y, Cpml.vec3.unit_x)
+   ViewMatrix:rotate   (ViewMatrix, Camera.rotation.x, Cpml.vec3.unit_y)
 
+   Shader:send("view_matrix",       "column", ViewMatrix)
+   Shader:send("projection_matrix", "column", PerspectiveMatrix)
+   
    love.graphics.setShader(Shader)
-		love.graphics.draw(Mesh)
-	love.graphics.setShader()
-end 
+   love.graphics.setColor(1, 1, 1)
+   love.graphics.setDepthMode("lequal", true)
+   love.graphics.setCanvas(DepthBuffer.canvas)
+   love.graphics.clear(1, 1, 1, 1, true, 1)
+
+	Shader:send("model_matrix", "column", ModelMatrix_1)
+   love.graphics.draw(Mesh)
+   
+   Shader:send("model_matrix", "column", ModelMatrix_2)
+   love.graphics.draw(Mesh)
+
+   love.graphics.setShader()
+   love.graphics.setDepthMode()
+   love.graphics.setCanvas()
+
+   love.graphics.draw(DepthBuffer.color)
+end
+
+function love.update(dt)
+   local movementVector = Cpml.vec3()
+
+   if love.keyboard.isDown("w") then movementVector.z = movementVector.z + 1 end
+   if love.keyboard.isDown("s") then movementVector.z = movementVector.z - 1 end
+
+   if love.keyboard.isDown("a") then movementVector.x = movementVector.x + 1 end
+   if love.keyboard.isDown("d") then movementVector.x = movementVector.x - 1 end
+
+   if love.keyboard.isDown("r") then movementVector.y = movementVector.y - 1 end
+   if love.keyboard.isDown("f") then movementVector.y = movementVector.y + 1 end
+
+   Camera.position = Camera.position + movementVector * 5 * dt
+end
+
+function love.mousepressed(x, y, button)
+	if button == 1 then
+		dragging = true
+		love.mouse.setRelativeMode(true)
+	end
+end
+
+function love.mousereleased(x, y, button)
+	if button == 1 then
+		dragging = false
+		love.mouse.setRelativeMode(false)
+	end
+end
+
+function love.mousemoved(x, y, dx, dy)
+   if dragging then
+      local rotationVector = Cpml.vec2(dx, dy)
+
+		Camera.rotation = Camera.rotation + rotationVector / 100
+	end
+end
+
--- shader.glsl
+++ shader.glsl
@@ -1,7 +1,16 @@
-uniform mat4 ModelMatrix;
-uniform mat4 ViewMatrix;
-uniform mat4 PerspectiveMatrix;
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
 
+#ifdef VERTEX
 vec4 position(mat4 transform_projection, vec4 vertex_position) {
-    return PerspectiveMatrix * ViewMatrix * ModelMatrix * vertex_position;
+	return projection_matrix * view_matrix * model_matrix * vertex_position;
 }
+#endif
+
+#ifdef PIXEL
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+	vec4 pixel = Texel(texture, texture_coords);
+	return color * pixel;
+}
+#endif
